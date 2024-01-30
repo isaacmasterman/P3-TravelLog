212a149ee65aeaf883eef78026c5bf8e7fe5fcae
isaacmasterman <142137796+isaacmasterman@users.noreply.github.com>
2024-01-31 02:26:43 +1100

    created Login and Signup pages with respective styling

---
+++ client/src/components/LoginPage.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { Box, Flex, Input, Button, Text, Center } from '@chakra-ui/react';
+import TravelLogLogo from '../assets/Images/TravelLog_Logo.png';
+import { useNavigate } from 'react-router-dom';
+
+// Import any other necessary components and hooks (authorisation)
+
+
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  // Add function to handle form submission here
+
+  return (
+    <Center bg="#EBEBEB" w="100vw" h="100vh">
+      <Flex direction="column" align="center" justify="center">
+        <Image src={TravelLogLogo} boxSize="200px" alt="Travel Log Logo" />
+        <Input 
+            placeholder="Username" 
+            type="text"
+            my="2" 
+            bg="#FFFFFF" 
+            borderColor="#292F33" 
+            borderRadius="md"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+        />
+        <Input 
+            placeholder="Password" 
+            type="password" 
+            my="2" 
+            bg="#FFFFFF" 
+            borderColor="#292F33" 
+            borderRadius="md"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} 
+        />
+        <Button 
+            bg="#292F33" 
+            color="#EBEBEB" 
+            my="2" 
+            _hover={{ bg: "#898989" }} 
+            onClick={/* form submission function here */}>
+                Login
+        </Button>
+        <Button 
+            bg="#292F33" 
+            color="#EBEBEB" 
+            my="2" 
+            _hover={{ bg: "#898989" }} 
+            onClick={() => navigate('/signup')}>
+                Signup
+        </Button>
+      </Flex>
+    </Center>
+  );
+};
+
+export default LoginPage;

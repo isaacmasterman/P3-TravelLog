c2476d913726a2983fb918158a120d6599ee3ada
isaacmasterman <142137796+isaacmasterman@users.noreply.github.com>
2024-02-06 19:19:17 +1100

    debugging

--- client/src/components/HomeMap.jsx
+++ client/src/components/HomeMap.jsx
@@ -29,31 +29,25 @@ export default function HomeMap() {
 
   if (!isLoaded) return <div>Loading...</div>;
   return (
-    <>
-
-      <Navbar />
-    {isDrawerOpen && <ListComponent />}
+  <>
+    <Navbar />
+      {isDrawerOpen && <ListComponent />}
     <Flex direction="row" height={`calc(100vh - 80px)`}>
-        <Box flex="1" overflowY="auto" >
-          {placeDetails && <PlaceCard placeData={placeDetails} />
-      <Flex direction="row" height={`calc(100vh - 80px)`}>
-        <Box flex="1" overflowY="auto">
-          {placeDetails && (
-            <>
-              {console.log("PlaceDetails structure:", placeDetails)}
-              <PlaceCard placeData={placeDetails} />
-            </>
-          )}
-
-        </Box>
-
-        <Box flex="3">
-          <PlacesAutocomplete onSelectPlace={handleSelectPlace} />
-          <Map />
-          {isLoading && <Spinner />}
-          {error && <Text color="red.500">{error}</Text>}
-        </Box>
-      </Flex>
-    </>
+      <Box flex="1" overflowY="auto">
+      {placeDetails && (
+        <>
+          {console.log("PlaceDetails structure:", placeDetails)}
+          <PlaceCard placeData={placeDetails} />
+        </>
+      )}
+      </Box>
+      <Box flex="3">
+        <PlacesAutocomplete onSelectPlace={handleSelectPlace} />
+        <Map />
+        {isLoading && <Spinner />}
+        {error && <Text color="red.500">{error}</Text>}
+      </Box>
+    </Flex>
+  </>
   );
 }

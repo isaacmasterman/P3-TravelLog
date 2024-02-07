e39505cc93797968e84e57a579e6d420169c84bd
isaacmasterman <142137796+isaacmasterman@users.noreply.github.com>
2024-02-07 23:46:42 +1100

    debugging deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping';
 
-module.exports = mongoose.connection;
-
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  
+  module.exports = mongoose.connection;
